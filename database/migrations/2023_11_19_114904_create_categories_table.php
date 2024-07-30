08bd6bc03b07bbbee32ad6a000d7d9d4ed50a72b
Ahmed Tamim <ahmedtamim19050@gmail.com>
2024-07-30 17:48:53 +0600

    issue

--- database/migrations/2023_11_19_114904_create_categories_table.php
+++ database/migrations/2023_11_19_114904_create_categories_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->string('name');
+            $table->string('slug')->unique();
+            $table->bigInteger('parent_id')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2024_11_19_073311_create_products_table.php
+++ database/migrations/2024_11_19_073311_create_products_table.php
@@ -16,22 +16,15 @@ return new class extends Migration
             $table->string('name');
             $table->string('image')->nullable();
             $table->string('sku')->nullable();
-            $table->string('barcode')->nullable();
+            $table->text('composition')->nullable();
+            $table->string('allergenes')->nullable();
             $table->bigInteger('price')->nullable();
-            $table->bigInteger('trade_price')->nullable();
-            $table->bigInteger('strip_price')->nullable();
-            $table->bigInteger('box_price')->nullable();
             $table->text('description')->nullable();
             $table->boolean('status')->default(true);
             $table->boolean('featured')->default(false);
             $table->integer('order')->nullable();
-            $table->integer('sold_unit')->default(0);
             $table->integer('quantity')->default(0);
-            $table->string('strength')->nullable();   
-            $table->string('scrapper_url')->nullable();   
             $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
-            $table->foreignId('supplier_id')->nullable()->constrained()->nullOnDelete();
-            $table->foreignId('generic_id')->nullable()->constrained()->nullOnDelete();
             $table->timestamps();
         });
     }
