3ed2e528cbb7d6d8bb2c2e6562d2cf0e3ac116d2
Mahfuzul Islam <mahfuzul1125@gmail.com>
2024-08-09 21:43:33 +0600

    restaurant

--- database/migrations/2024_08_09_171407_create_restaurant_zone_table.php
+++ database/migrations/2024_08_09_171407_create_restaurant_zone_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('restaurant_zones');
+        Schema::dropIfExists('restaurant_zone');
     }
 };
