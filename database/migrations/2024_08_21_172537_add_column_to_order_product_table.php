024568186cfadac42671a176c7505eea652389d9
Mahfuzul Islam <mahfuzul1125@gmail.com>
2024-08-21 17:31:23 +0600

    variation again

--- database/migrations/2024_08_21_172537_add_column_to_order_product_table.php
+++ database/migrations/2024_08_21_172537_add_column_to_order_product_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('order_product', function (Blueprint $table) {
-            $table->text('variation');
+            $table->text('variation')->nullable();
         });
     }
 
