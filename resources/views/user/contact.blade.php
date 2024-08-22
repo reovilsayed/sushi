4d973d822ead8105418ec67099f78075c602a27e
Abdur Rahman <rahmanabdur64870@gmail.com>
2024-08-23 00:24:55 +0600

    map

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -71,10 +71,8 @@
                         class="d-xl-block user-logout-button mt-3">Submit</button>
                     </form> --}}
 
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2704.535575207828!2d5.0314998!3d47.3234108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f29d94bd63fdcb%3A0x5c3d38bb2bb2ac71!2s25%20Pl.%20Darcy%2C%2021000%20Dijon%2C%20France!5e0!3m2!1sen!2sae!4v1709789861376!5m2!1sen!2sae"
-                        width="1300" height="800" style="border:0;" allowfullscreen="" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div id="map" style="height: 500px; width: 100%;"></div>
+
 
                     {{-- <div id="map-nos"></div> --}}
                 </div><!-- End Contact Form -->
@@ -87,37 +85,41 @@
 
     @push('js')
         <script>
-            function initMapNOS() {
+            function initMap() {
+                // Define the locations
                 const locations = [{
-                        lat: 47.323430,
-                        lng: 5.031508
-                    }, // New York City
+                        lat: 47.631498,
+                        lng: 6.856458
+                    }, // Central Sushi Belfort
                     {
-                        lat: 47.245721,
-                        lng: 6.028465
-                    }, // Los Angeles
+                        lat: 47.2452395,
+                        lng: 6.0284224
+                    }, // Besançon
                     {
-                        lat: 47.631816,
-                        lng: 6.856420
-                    } // London
+                        lat: 47.3234108,
+                        lng: 5.0314998
+                    } // Dijon
                 ];
 
-                const map = new google.maps.Map(document.getElementById('map-nos'), {
+                // Create a map centered on the average of the three locations
+                const map = new google.maps.Map(document.getElementById("map"), {
                     zoom: 7,
-                    center: locations[0]
+                    center: {
+                        lat: 47.4,
+                        lng: 6.0
+                    }
                 });
 
-                locations.forEach((location) => {
+                // Add markers to the map
+                locations.forEach(location => {
                     new google.maps.Marker({
                         position: location,
                         map: map
                     });
                 });
             }
-
-            initMapNOS();
-        </script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMapNOS&v=weekly" async defer>
+            // Initialize the map
+            google.maps.event.addDomListener(window, 'load', initMap);
         </script>
     @endpush
 </x-user>
