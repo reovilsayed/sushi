bcc6e090dd8f492d37722d7e2943255a08f158ca
sayed khan <reovilsayed@gmail.com>
2024-09-12 11:09:35 +0600

    dfd

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img src="{{ asset(Storage::url(Settings::setting('site.logo'))) }}" class="logo" alt="Laravel Logo">
-            @else
-                {{ $slot }}
-            @endif
+                <img src="{{ asset(Storage::url(Settings::setting('site.logo'))) }}" class="logo" alt="central sushi">
         </a>
     </td>
 </tr>
